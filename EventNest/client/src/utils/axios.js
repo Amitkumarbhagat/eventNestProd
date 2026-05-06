e409c839d076f5cee6a670509d1b4ae3092ef248
Amit Bhagat <78423707+Amitkumarbhagat@users.noreply.github.com>
2026-05-06 23:33:55 +0530

    Update API base URL to production endpoint

--- EventNest/client/src/utils/axios.js
+++ EventNest/client/src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     baseURL:
         import.meta.env.VITE_API_URL ||
-        'http://localhost:5000/api',
+        'https://eventnest-53.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
