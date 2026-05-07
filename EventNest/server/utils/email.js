396bdffc655be7a4fcf0a0df538ee6e5c6b394ad
Amit Bhagat <78423707+Amitkumarbhagat@users.noreply.github.com>
2026-05-07 15:28:56 +0530

    Update email.js

--- EventNest/server/utils/email.js
+++ EventNest/server/utils/email.js
@@ -14,24 +14,19 @@
 
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
-const dns = require('dns');
+//const dns = require('dns');
 
 dotenv.config();
-
-dns.setDefaultResultOrder('ipv4first');
+//dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
-    family: 4,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.EMAIL_PASS,
     },
-    tls: {
-        rejectUnauthorized: false
-    }
 });
 
 
